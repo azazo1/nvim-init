77590e83bd1f3884cb97562487cd414b07bf14ad
azazo1 <azazo1@qq.com>
2024-12-08 13:57:55 +0800

    more buffer keymap

--- lua/which_key/keys.lua
+++ lua/which_key/keys.lua
@@ -54,34 +54,49 @@ if not vim.g.vscode then
     }
     -- bufferline.nvim
     local bufferline = require("bufferline")
-    local close_buffer_asking = function() -- 关闭当前缓冲区, 并带有响应的询问.
-        if vim.bo.modified then
+    local close_buffer_asking = function(buf)
+        -- 关闭缓冲区, 并带有相应的询问.
+        -- 如果参数不提供, 默认使用当前的缓冲区.
+        -- 返回关闭操作是否真正执行.
+        buf = buf or vim.api.nvim_get_current_buf()
+        if vim.api.nvim_get_option_value('modified', {
+            buf = buf
+        }) then
             -- 询问.
-            local choise = vim.fn.confirm('Buffer modified, sure to close?', '&Yes,\n&No\nor &Write and close', 3)
-            if choise == 1 then
-                bufferline.unpin_and_close()
+            local filename = vim.api.nvim_buf_get_name(buf)
+            local choise = vim.fn.confirm(filename .. '\nBuffer modified, sure to close?',
+                '&Yes,\n&No\nor &Write and close', 3)
+            if choise == 1 then -- 强制关闭.
+                vim.api.nvim_set_option_value('modified', false, {
+                    buf = buf
+                })
+                bufferline.unpin_and_close(buf)
             elseif choise == 3 then
                 -- 先写入文件, 使用代码安静写入.
-                local bufnr = vim.api.nvim_get_current_buf()
-                local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-                local filename = vim.api.nvim_buf_get_name(bufnr)
+                local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
                 vim.fn.writefile(lines, filename)
-                vim.bo.modified = false -- 标记已经写入.
+                vim.api.nvim_set_option_value('modified', false, {
+                    buf = buf
+                }) -- 标记已经写入.
                 -- 关闭缓冲区.
-                bufferline.unpin_and_close()
+                bufferline.unpin_and_close(buf)
+            else
+                return false
             end
-        else
-            bufferline.unpin_and_close()
+        else -- 缓冲区没有修改.
+            bufferline.unpin_and_close(buf)
         end
+        return true
     end
-    local get_loaded_buffer_count = function() -- 查看当前已加载的缓冲区数量.
-        local count = 0
+    local get_valid_buffers = function()
+        -- 获取当前有效的缓冲区.
+        local t = {}
         for _, b in ipairs(vim.api.nvim_list_bufs()) do
-            if vim.api.nvim_buf_is_loaded(b) then
-                count = 1 + count
+            if vim.api.nvim_buf_is_valid(b) then
+                table.insert(t, b)
             end
         end
-        return count
+        return t
     end
     wk.add { -- Buffer 操作.
         "<leader>b",
@@ -91,28 +106,44 @@ if not vim.g.vscode then
         { -- 转到左边的 Buffer.
             "<leader>bh",
             "<Cmd>BufferLineCyclePrev<CR>",
-            {"<A-,>", "<Cmd>BufferLineCyclePrev<CR>"},
-            desc = "Buffer on Left",
+            {
+                "<A-,>",
+                "<Cmd>BufferLineCyclePrev<CR>",
+                mode = {"n", "i"}
+            },
+            desc = "Go to Buffer on Left",
             icon = "󰙤"
         },
         { -- 转到右边的 Buffer.
             "<leader>bl",
             "<Cmd>BufferLineCycleNext<CR>",
-            {"<A-.>", "<Cmd>BufferLineCycleNext<CR>"},
-            desc = "Buffer on Right",
+            {
+                "<A-.>",
+                "<Cmd>BufferLineCycleNext<CR>",
+                mode = {"n", "i"}
+            },
+            desc = "Go to Buffer on Right",
             icon = "󰙢"
         },
         { -- 当前 Buffer 向左移.
             "<leader>bj",
             "<Cmd>BufferLineMovePrev<CR>",
-            {"<A-<>", "<Cmd>BufferLineMovePrev<CR>"},
+            {
+                "<A-<>",
+                "<Cmd>BufferLineMovePrev<CR>",
+                mode = {"n", "i"}
+            },
             desc = "Buffer Move Left",
             icon = ""
         },
         { -- 当前 Buffer 向右移.
             "<leader>bk",
             "<Cmd>BufferLineMoveNext<CR>",
-            {"<A->>", "<Cmd>BufferLineMoveNext<CR>"},
+            {
+                "<A->>",
+                "<Cmd>BufferLineMoveNext<CR>",
+                mode = {"n", "i"}
+            },
             desc = "Buffer Move Right",
             icon = ""
         },
@@ -133,11 +164,10 @@ if not vim.g.vscode then
             close_buffer_asking,
             desc = "Close Buffer",
             icon = "󰅙",
-            {
+            { -- 关闭当前 Buffer 并尝试退出.
                 "<leader>q",
                 function()
-                    if (get_loaded_buffer_count() == 1 -- 只有当前这个缓冲区被加载.
-                    and vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) == "" -- 当前缓冲区是无名缓冲区.
+                    if (vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) == "" -- 当前缓冲区是无名缓冲区.
                     and not vim.bo.modified -- 当前无名缓冲区没有被修改过.
                     ) then
                         vim.cmd [[q]]
@@ -146,6 +176,22 @@ if not vim.g.vscode then
                     end
                 end,
                 desc = "Close Buffer and Quitting"
+            },
+            { -- 关闭所有 Buffer 并退出.
+                "<leader>Q",
+                function()
+                    local quit = true
+                    for _, b in ipairs(get_valid_buffers()) do
+                        if not close_buffer_asking(b) then
+                            quit = false -- 有一个取消就中断.
+                            break
+                        end
+                    end
+                    if quit then
+                        vim.cmd [[q]]
+                    end
+                end,
+                desc = "Close All Buffer and Quitting"
             }
         }
     }
