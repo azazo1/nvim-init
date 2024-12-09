13d457e69f2c128dbed4d8d5daf1b9793dcc222c
azazo1 <azazo1@qq.com>
2024-12-09 15:56:54 +0800

    replace `\t` with `    `

--- lua/which_key/keys.lua
+++ lua/which_key/keys.lua
@@ -29,87 +29,87 @@ wk.add({ -- 保存文件.
 })
 
 wk.add { -- 撤销操作.
-	"<^-Z>",
-	"<Cmd>u<CR>",
-	desc = "Undo",
-	icon = "",
-	mode = "n",
+    "<^-Z>",
+    "<Cmd>u<CR>",
+    desc = "Undo",
+    icon = "",
+    mode = "n",
 }
 
 wk.add { -- 显示当前文件信息.
-	"<leader>i",
-	"<Cmd>file<CR>",
-	mode = "n",
-	desc = "Show File Info",
-	icon = "",
+    "<leader>i",
+    "<Cmd>file<CR>",
+    mode = "n",
+    desc = "Show File Info",
+    icon = "",
 }
 
 if not vim.g.vscode then
-	-- 窗口焦点移动.
-	wk.add {
-		mode = {"i", "n"},
-		group = "Move Focus",
-		{ -- 向左.
-			"<C-h>",
-			"<Esc><C-w>h",
-			desc = "Move Focus Left",
-		},
-		{ -- 向右.
-			"<C-l>",
-			"<Esc><C-w>l",
-			desc = "Move Focus Right",
-		},
-		{ -- 向下.
-			"<C-j>",
-			"<Esc><C-w>j",
-			desc = "Move Focus Down",
-		},
-		{ --向上.
-			"<C-k>",
-			"<Esc><C-w>k",
-			desc = "Move Focus Up",
-		}
-	}
+    -- 窗口焦点移动.
+    wk.add {
+        mode = {"i", "n"},
+        group = "Move Focus",
+        { -- 向左.
+            "<C-h>",
+            "<Esc><C-w>h",
+            desc = "Move Focus Left",
+        },
+        { -- 向右.
+            "<C-l>",
+            "<Esc><C-w>l",
+            desc = "Move Focus Right",
+        },
+        { -- 向下.
+            "<C-j>",
+            "<Esc><C-w>j",
+            desc = "Move Focus Down",
+        },
+        { --向上.
+            "<C-k>",
+            "<Esc><C-w>k",
+            desc = "Move Focus Up",
+        }
+    }
     -- nvim-tree.lua
     local nvim_tree_api = require("nvim-tree.api")
-	local toggle_quit_on_open = function()
-		local config = require('nvim-tree').config
-		config.actions.open_file.quit_on_open = not config.actions.open_file.quit_on_open
-		require('nvim-tree').setup(config)
-		if config.actions.open_file.quit_on_open then
-			vim.print("nvim-tree will quit_on_open")
-		else
-			vim.print("nvim-tree will not quit_on_open")
-		end
-	end
-	local toggle_explorer = function()
-		-- 如果焦点不在文件树且文件树打开, 那么先不关闭, 先聚焦.
-		if nvim_tree_api.tree.is_visible({
-			any_tabpage = true
-		}) and nvim_tree_api.tree.is_tree_buf() then
-			nvim_tree_api.tree.close()
-		else
-			nvim_tree_api.tree.open()
-		end
-		vim.cmd [[cd]] -- 输出当前路径
-	end
+    local toggle_quit_on_open = function()
+        local config = require('nvim-tree').config
+        config.actions.open_file.quit_on_open = not config.actions.open_file.quit_on_open
+        require('nvim-tree').setup(config)
+        if config.actions.open_file.quit_on_open then
+            vim.print("nvim-tree will quit_on_open")
+        else
+            vim.print("nvim-tree will not quit_on_open")
+        end
+    end
+    local toggle_explorer = function()
+        -- 如果焦点不在文件树且文件树打开, 那么先不关闭, 先聚焦.
+        if nvim_tree_api.tree.is_visible({
+            any_tabpage = true
+        }) and nvim_tree_api.tree.is_tree_buf() then
+            nvim_tree_api.tree.close()
+        else
+            nvim_tree_api.tree.open()
+        end
+        vim.cmd [[cd]] -- 输出当前路径
+    end
     wk.add { -- 文件树键位.
-		"<leader>e",
-		mode = "n",
-		group = "Explorer",
-		icon = "󱏒",
-		{ -- 切换文件侧边栏打开与关闭.
-			"<leader>ee",
-			toggle_explorer,
-			desc = "Toggle Explorer",
-			{"<A-e>", toggle_explorer}
-		},
-		{ -- 切换: 文件树在打开文件后自动关闭.
-			"<leader>eE",
-			toggle_quit_on_open,
-			desc = "Toggle Quit on Open",
-		}
-	}
+        "<leader>e",
+        mode = "n",
+        group = "Explorer",
+        icon = "󱏒",
+        { -- 切换文件侧边栏打开与关闭.
+            "<leader>ee",
+            toggle_explorer,
+            desc = "Toggle Explorer",
+            {"<A-e>", toggle_explorer}
+        },
+        { -- 切换: 文件树在打开文件后自动关闭.
+            "<leader>eE",
+            toggle_quit_on_open,
+            desc = "Toggle Quit on Open",
+        }
+    }
     -- bufferline.nvim
     local bufferline = require("bufferline")
     local close_buffer_asking = function(buf)
@@ -150,7 +150,7 @@ if not vim.g.vscode then
         -- 获取当前打开的缓冲区, 会跳过文件树 Buffer 界面.
         local t = {}
         for _, e in ipairs(bufferline.get_elements().elements) do
-			table.insert(t, e.id)
+            table.insert(t, e.id)
         end
         return t
     end
@@ -224,14 +224,14 @@ if not vim.g.vscode then
                 "<leader>q",
                 function()
                     if (
-						#get_expected_buffers() == 1 -- 只有一个有效且被加载的 buffer.
-						and vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) == "" -- 当前缓冲区是无名缓冲区.
-						and not vim.bo.modified -- 当前无名缓冲区没有被修改过.
+                        #get_expected_buffers() == 1 -- 只有一个有效且被加载的 buffer.
+                        and vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) == "" -- 当前缓冲区是无名缓冲区.
+                        and not vim.bo.modified -- 当前无名缓冲区没有被修改过.
                     ) then
-						vim.api.nvim_feedkeys(
-							vim.api.nvim_replace_termcodes("<Cmd>q<CR>", true, false, true),
-							'n', true
-						)
+                        vim.api.nvim_feedkeys(
+                            vim.api.nvim_replace_termcodes("<Cmd>q<CR>", true, false, true),
+                            'n', true
+                        )
                     else
                         close_buffer_asking()
                     end
@@ -249,15 +249,15 @@ if not vim.g.vscode then
                         end
                     end
                     if quit then
-						vim.api.nvim_feedkeys(
-							vim.api.nvim_replace_termcodes("<Cmd>qa!<CR>",
-								true, -- 是否启动键映射.
-								false, -- 是否在模式中等待.
-								true -- 是否支持特殊按键, 比如 <C-w>.
-							),
-							'n', -- 模式 (normal).
-							true -- 是否立刻执行.
-						)
+                        vim.api.nvim_feedkeys(
+                            vim.api.nvim_replace_termcodes("<Cmd>qa!<CR>",
+                                true, -- 是否启动键映射.
+                                false, -- 是否在模式中等待.
+                                true -- 是否支持特殊按键, 比如 <C-w>.
+                            ),
+                            'n', -- 模式 (normal).
+                            true -- 是否立刻执行.
+                        )
                     end
                 end,
                 desc = "Close All Buffer and Quitting"
@@ -283,8 +283,8 @@ if not vim.g.vscode then
         { -- 比较当前文件的修改 (与上一次 commit 比较).
             "<leader>hd",
             function()
-				gitsigns.diffthis('~1')
-			end,
+                gitsigns.diffthis('~1')
+            end,
             desc = "Diff This",
             icon = ""
         },
@@ -296,30 +296,30 @@ if not vim.g.vscode then
         },
         { -- 查看改动块, 激活两次进入改动快窗口内部进行查看与复制.
             "<leader>hp",
-			gitsigns.preview_hunk,
+            gitsigns.preview_hunk,
             desc = "Preview Hunk",
             icon = "󱀂"
         },
-		{ -- 跳转到上一个改动块.
-			"<leader>hk",
-			function()
-				gitsigns.nav_hunk('prev',
-					{preview = true, target='all', wrap=true}
-				)
-			end,
-			desc = "Go to Previous Hunk",
-			icon = ""
-		},
-		{ -- 跳转到下一个改动块.
-			"<leader>hj",
-			function()
-				gitsigns.nav_hunk('next',
-					{preview = true, target='all', wrap=true}
-				)
-			end,
-			desc = "Go to Next Hunk",
-			icon = ""
-		},
+        { -- 跳转到上一个改动块.
+            "<leader>hk",
+            function()
+                gitsigns.nav_hunk('prev',
+                    {preview = true, target='all', wrap=true}
+                )
+            end,
+            desc = "Go to Previous Hunk",
+            icon = ""
+        },
+        { -- 跳转到下一个改动块.
+            "<leader>hj",
+            function()
+                gitsigns.nav_hunk('next',
+                    {preview = true, target='all', wrap=true}
+                )
+            end,
+            desc = "Go to Next Hunk",
+            icon = ""
+        },
         { -- 暂存/取消暂存改动块.
             "<leader>hs",
             gitsigns.stage_hunk,
@@ -345,52 +345,52 @@ if not vim.g.vscode then
             icon = ""
         }
     }
-	-- telescope.nvim
-	local telescope_builtin = require("telescope.builtin")
-	wk.add { -- Telescope 操作.
-		"<leader>f",
-		mode = "n",
-		group = "Telescope",
-		icon = "",
-		{ -- Telescope 界面.
-			"<leader>ff",
-			"<Cmd>Telescope<CR>",
-			desc = "Open Telescope Window",
-			icon = "",
-		},
-		{ -- 查找文件.
-			"<leader>fd",
-			"<Cmd>Telescope find_files<CR>",
-			desc = "Telescope Find Files",
-			icon = "󰈞",
-		},
-		{ -- 实时查找内容. todo 继承上一次搜索的内容.
-			"<leader>fg",
-			"<Cmd>Telescope live_grep<CR>",
-			desc = "Telescope Live Grep",
-			icon = "",
-		},
-		{ -- 实时查找光标下的内容.
-			"<leader>fc",
-			function()
-				telescope_builtin.grep_string({
-					use_regex = true
-				})
-			end,
-			desc = "Grep Cursor Word",
-			icon = "󰗧",
-		},
-		{ -- 查看所有键位.
-			"<leader>fk",
-			"<Cmd>Telescope keymaps<CR>",
-			desc = "Keymaps",
-			icon = "",
-		},
-		{ -- 查看帮助文档列表.
-			"<leader>fh",
-			"<Cmd>Telescope help_tags<CR>",
-			desc = "Help Docs",
-			icon = "󰈙",
-		}
-	}
+    -- telescope.nvim
+    local telescope_builtin = require("telescope.builtin")
+    wk.add { -- Telescope 操作.
+        "<leader>f",
+        mode = "n",
+        group = "Telescope",
+        icon = "",
+        { -- Telescope 界面.
+            "<leader>ff",
+            "<Cmd>Telescope<CR>",
+            desc = "Open Telescope Window",
+            icon = "",
+        },
+        { -- 查找文件.
+            "<leader>fd",
+            "<Cmd>Telescope find_files<CR>",
+            desc = "Telescope Find Files",
+            icon = "󰈞",
+        },
+        { -- 实时查找内容. todo 继承上一次搜索的内容.
+            "<leader>fg",
+            "<Cmd>Telescope live_grep<CR>",
+            desc = "Telescope Live Grep",
+            icon = "",
+        },
+        { -- 实时查找光标下的内容.
+            "<leader>fc",
+            function()
+                telescope_builtin.grep_string({
+                    use_regex = true
+                })
+            end,
+            desc = "Grep Cursor Word",
+            icon = "󰗧",
+        },
+        { -- 查看所有键位.
+            "<leader>fk",
+            "<Cmd>Telescope keymaps<CR>",
+            desc = "Keymaps",
+            icon = "",
+        },
+        { -- 查看帮助文档列表.
+            "<leader>fh",
+            "<Cmd>Telescope help_tags<CR>",
+            desc = "Help Docs",
+            icon = "󰈙",
+        }
+    }
 end
