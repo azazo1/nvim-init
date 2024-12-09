5fb17c1214e080cc8eba1e1115e84ba8215d20d3
azazo1 <azazo1@qq.com>
2024-12-09 15:49:58 +0800

    telescope keymap

--- lua/which_key/keys.lua
+++ lua/which_key/keys.lua
@@ -353,18 +353,18 @@ if not vim.g.vscode then
 		group = "Telescope",
 		icon = "",
 		{ -- Telescope 界面.
-			"<leader>ft",
+			"<leader>ff",
 			"<Cmd>Telescope<CR>",
 			desc = "Open Telescope Window",
 			icon = "",
 		},
 		{ -- 查找文件.
-			"<leader>ff",
+			"<leader>fd",
 			"<Cmd>Telescope find_files<CR>",
 			desc = "Telescope Find Files",
 			icon = "󰈞",
 		},
-		{ -- 实时查找内容.
+		{ -- 实时查找内容. todo 继承上一次搜索的内容.
 			"<leader>fg",
 			"<Cmd>Telescope live_grep<CR>",
 			desc = "Telescope Live Grep",
@@ -379,6 +379,18 @@ if not vim.g.vscode then
 			end,
 			desc = "Grep Cursor Word",
 			icon = "󰗧",
+		},
+		{ -- 查看所有键位.
+			"<leader>fk",
+			"<Cmd>Telescope keymaps<CR>",
+			desc = "Keymaps",
+			icon = "",
+		},
+		{ -- 查看帮助文档列表.
+			"<leader>fh",
+			"<Cmd>Telescope help_tags<CR>",
+			desc = "Help Docs",
+			icon = "󰈙",
 		}
 	}
 end
