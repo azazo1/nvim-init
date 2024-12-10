3b3d9e580fdd9d2ca1ceb0ee3075862243a00c35
azazo1 <azazo1@qq.com>
2024-12-10 18:06:59 +0800

    nvim-cmp 对命令输入的提示; nvim-cmp 提示窗口边框;

--- lua/lazy_plugins/lsp_related.lua
+++ lua/lazy_plugins/lsp_related.lua
@@ -14,14 +14,16 @@ table.insert(p, {
         -- require 的顺序就要这样放.
         require("mason").setup()
         require("mason-lspconfig").setup {
-            ensure_installed = {"lua_ls"},
+            ensure_installed = { -- 确保要安装的 LSP.
+				"lua_ls", "pyright", "rust_analyzer"
+			},
             automatic_installation = true
         }
         require("mason-lspconfig").setup_handlers {
             -- The first entry (without a key) will be the default handler
             -- and will be called for each installed server that doesn't have
             -- a dedicated handler.
-            function(server_name) -- default handler (optional)
+            function(server_name) -- default handler (optional), 默认 LSP 配置设置.
                 require("lspconfig")[server_name].setup {}
             end,
             -- Next, you can provide a dedicated handler for specific servers.
@@ -29,6 +31,7 @@ table.insert(p, {
             -- ["rust_analyzer"] = function()
             --     require("rust-tools").setup {}
             -- end
+			-- 下面是自定义 LSP 配置的设置.
             ["lua_ls"] = function()
                 local lspconfig = require("lspconfig")
                 lspconfig.lua_ls.setup {
@@ -50,7 +53,8 @@ table.insert(p, { -- 自动补全相关设置
     dependencies = {
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path'
+        'hrsh7th/cmp-path',
+		'hrsh7th/cmp-cmdline'
     },
     config = function()
         local cmp = require("cmp")
@@ -59,7 +63,8 @@ table.insert(p, { -- 自动补全相关设置
                 { name = 'nvim_lsp' }, -- 自动补全 lsp 提供的内容.
                 { name = "path" }, -- 自动补全路径.
                 -- { name = "vsnip" }, -- snippets, https://github.com/hrsh7th/vim-vsnip
-                { name = "buffer" }
+                { name = "buffer" },
+				{ name = "cmdline" },
             }),
             mapping = cmp.mapping.preset.insert({
                 -- ['<C-Space>'] = cmp.mapping.complete(), -- 实际上没有必要, <C-n> 和 <C-p> 就能直接唤出.
@@ -67,7 +72,30 @@ table.insert(p, { -- 自动补全相关设置
                 ['<CR>'] = cmp.mapping.confirm({ select = true }), -- 确认补全, Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 ['<Tab>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
             }),
+			window = { -- 代码提示的窗口带边框.
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			experimental = { -- 代码提示在光标之后的虚拟文字.
+				ghost_text = true
+			}
         }
+		local opts = {
+			mapping = cmp.mapping.preset.cmdline(), -- 覆盖原生提示的键位, 达到禁用原生提示的作用.
+			sources = {
+				{ name = "buffer" }
+			}
+		}
+		cmp.setup.cmdline('?', opts)
+		cmp.setup.cmdline('/', opts)
+		cmp.setup.cmdline(':', {
+			mapping = cmp.mapping.preset.cmdline(), -- 同上.
+			sources = cmp.config.sources({
+				{ name = "cmdline" }
+			}, {
+				{ name = "path" }
+			})
+		})
     end,
     cond = not vim.g.vscode
 })
