b5fc8ab0412832d5799c498d08cb9c6b1957f702
azazo1 <azazo1@qq.com>
2025-12-04 19:30:20 +0800

    basic configs

--- init.lua
+++ init.lua
@@ -1,3 +1,29 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.g.autoformat = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+-- 右侧参考线
+vim.wo.colorcolumn = "85"
+-- 缩进字符
+vim.o.tabstop = 4
+vim.bo.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftround = true
+-- >> << 时移动长度
+vim.o.shiftwidth = 4
+vim.bo.shiftwidth = 4
+-- 空格替代
+-- tabvim.o.expandtab = true
+vim.bo.expandtab = true
+-- 新行对齐当前行
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.smartindent = true
+-- 搜索大小写不敏感, 除非包含大写
+vim.o.ignorecase = true
+vim.o.smartcase = true
+-- 搜索不要高亮
+vim.o.hlsearch = false
+vim.o.incsearch = true
