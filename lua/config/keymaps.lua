6240f8df0c0719952fb7ba2fd3d8d531f8c8f61c
azazo1 <azazo1@qq.com>
2026-05-06 21:09:42 +0800

    not auto sync clipboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({ "n", "x" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
+vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set("x", "<leader>p", '"_d"+P', { desc = "Paste from system clipboard" })
