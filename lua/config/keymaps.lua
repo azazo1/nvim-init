67bf89cd6e9bb638057e62ce4aed11403cb1df7d
azazo1 <azazo1@qq.com>
2026-06-17 11:23:16 +0800

    ignore F1/2/3/4/5/6/... keys and its modified version keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,41 @@ vim.keymap.set({ "n", "x" }, "<leader>y", '"+y', { desc = "Yank to system clipbo
 vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
 vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
 vim.keymap.set("x", "<leader>p", '"_d"+P', { desc = "Paste from system clipboard" })
+
+local function ignore_function_key(lhs)
+  pcall(vim.keymap.set, { "n", "x", "s", "o", "i", "c", "t" }, lhs, "<Nop>", {
+    silent = true,
+    desc = "Ignore function key",
+  })
+end
+
+for key = 1, 24 do
+  local key_name = "F" .. key
+  local modifiers = { "S", "C", "M", "D" }
+  local prefix = {}
+  local used = {}
+
+  ignore_function_key("<" .. key_name .. ">")
+
+  local function map_modifier_orders()
+    if #prefix > 0 then
+      ignore_function_key("<" .. table.concat(prefix, "-") .. "-" .. key_name .. ">")
+    end
+
+    if #prefix == #modifiers then
+      return
+    end
+
+    for _, modifier in ipairs(modifiers) do
+      if not used[modifier] then
+        used[modifier] = true
+        prefix[#prefix + 1] = modifier
+        map_modifier_orders()
+        prefix[#prefix] = nil
+        used[modifier] = nil
+      end
+    end
+  end
+
+  map_modifier_orders()
+end
